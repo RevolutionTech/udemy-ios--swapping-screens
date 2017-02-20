b73a8e1fdbd87aaeab5c941f70d7c243413512c6
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-02-20 14:54:11 -0800

    Create segues to switch between ViewControllers
    and also pass data between VCs

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  SwappingScreens
+//
+//  Created by Lucas Connors on 2/20/17.
+//  Copyright © 2017 Revolution Technologies. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
